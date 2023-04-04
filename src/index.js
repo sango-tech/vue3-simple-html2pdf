bf8e2c002cd63a12798a408b916a1ac11fe9f570
Gergely Dremák <gergely089@gmail.com>
2023-04-04 14:54:36 +0200

    Update index.js
    This allows Vite to actually use the package.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import Vue3SimpleHtml2pdf from 'vue3-simple-html2pdf/src/vue3-simple-html2pdf'
+import Vue3SimpleHtml2pdf from 'vue3-simple-html2pdf/src/vue3-simple-html2pdf.vue'
 const install = function (app) {
   app.component('Vue3SimpleHtml2pdf', Vue3SimpleHtml2pdf);
 }
